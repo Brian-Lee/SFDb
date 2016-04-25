49af7e9980a7f719932afc336c848b818af59a35
Brian-Lee <BrianTrewLee@gmail.com>
2016-04-24 22:21:41 -0700

    started to incorporate onLocationChange changes from SFDistricts3

--- app/src/main/java/com/safesanfrancisco/sfdb/MainActivity.java
+++ app/src/main/java/com/safesanfrancisco/sfdb/MainActivity.java
@@ -33,11 +33,11 @@ import com.safesanfrancisco.sfdb.mapPoint;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     //public class MainActivity extends AppCompatActivity  {
-    protected LocationManager locationManager;
+    //protected LocationManager locationManager;
     protected LocationListener locationListener;
     protected Context context;
     TextView txtLat;
-    String lat;
+    //String lat;
     String provider;
     protected String latitude, longitude;
     protected boolean gps_enabled, network_enabled;
@@ -51,6 +51,59 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+
+
+
+
+    private LocationManager locationManager;
+    public TextView textView;
+    public Double lat;
+    public Double lon;
+    //public mapPoint x;
+    public Double metersInMile = 1609.34;
+
+    //public FloatStringString[] targetDistrictsArray = new FloatStringString[151];
+    public District[] targetDistricts = new District[151];
+
+    public float shortestDistance1 = 999999999;
+    public float shortestDistance2 = 999999999;
+    public float shortestDistance3 = 999999999;
+    public float shortestDistance4 = 999999999;
+    public float shortestDistance5 = 999999999;
+    public float shortestDistance6 = 999999999;
+    public float shortestDistance7 = 999999999;
+    public float shortestDistance8 = 999999999;
+    public float shortestDistance9 = 999999999;
+    public float shortestDistance10 = 999999999;
+
+    public String closestDistrict1 = "";
+    public String closestDistrict2 = "";
+    public String closestDistrict3 = "";
+    public String closestDistrict4 = "";
+    public String closestDistrict5 = "";
+    public String closestDistrict6 = "";
+    public String closestDistrict7 = "";
+    public String closestDistrict8 = "";
+    public String closestDistrict9 = "";
+    public String closestDistrict10 = "";
+
+    public String targetZoomLevel1 = "z";
+    public String targetZoomLevel2 = "z";
+    public String targetZoomLevel3 = "z";
+    public String targetZoomLevel4 = "z";
+    public String targetZoomLevel5 = "z";
+    public String targetZoomLevel6 = "z";
+    public String targetZoomLevel7 = "z";
+    public String targetZoomLevel8 = "z";
+    public String targetZoomLevel9 = "z";
+    public String targetZoomLevel10 = "z";
+
+    public Location targetLocation = new Location("");//provider name is unecessary
+    public String targetDistrict = "";
+    public float targetDistance = 999999999;
+
+    public String targetZoomLevel = "0z";
 
 
     @Override
@@ -79,12 +132,67 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @Override
-    public void onLocationChanged(Location location) {
+    public void onLocationChanged(Location currentLocation) {
+
+
+        String district = "Not San Francisco";
+        lat = currentLocation.getLatitude();
+        lon = currentLocation.getLongitude();
         txtLat = (TextView) findViewById(R.id.textview1);
 
-        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude() + "\n" + "\n" + SFD0 + "\n");
+        txtLat.setText("Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude() + "\n" + "\n" + SFD0 + "\n");
 
 
+
+
+
+
+
+
+        //Location.distanceBetween(double startLatitude, double startLongitude, double endLatitude, double endLongitude, float[] results)
+
+
+        //if (37.747900 < lat < 37.765476) and ( -122.496239 < long < -122.476327) then central sunset
+
+        if (37.734122 < lat && lat < 37.766222 && -122.496239 < lon && lon < -122.457572)  {district = "Inner Sunset";}
+        if (37.747900 < lat && lat < 37.765476 && -122.496239 < lon && lon < -122.476327)  {district = "Central Sunset";}
+        if (37.734122 < lat && lat < 37.764594 && -122.510230 < lon && lon < -122.493964)  {district = "Outer Sunset";}
+        if (37.764119 < lat && lat < 37.774093 && -122.510959 < lon && lon < -122.453195)  {district = "Golden Gate Park";}
+
+
+
+
+        String str = "Latitude: "+currentLocation.getLatitude()+" \nLongitude: "+currentLocation.getLongitude();
+        str += "\n" + "Altitude"+currentLocation.getAltitude();
+        //str += "\n" + "Nearest Address"+location.getAddress();
+        str += "\n" + district;
+
+        str += "\n\n\n";
+        str += "targetDistrict=\n";
+     /////44   for (int i=0;i<125; i++)
+     /////44   {
+     /////44       str += targetDistricts[i].citySection +" - ";
+     /////44       str += targetDistricts[i].district+" - "+targetDistricts[i].distance+" miles\n";
+      /////44  }
+
+        str += "\n\n\n";
+        str += "targetDistrict=\n";
+        //str += targetDistricts;
+/////44        str += targetDistricts[0].district+" - "+targetDistricts[0].distance+" miles\n";
+
+        str += "\n";
+        str += "\n" ;
+
+
+
+
+        //@strings/see_alert_message_for_logitude_and_latitude_ = "xxx";
+        ////	R.strings.main_activity.see_alert_message_for_logitude_and_latitude_ = "xxx";
+
+        ////textView.setText("yyy");
+        txtLat.setText(str);//textView.setText(str);
+
+        //Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
     }
 
     @Override
